5e02c9cc2f91cb49cb18a85f682d688c9251855f
zxcvne <nhs8421@gmail.com>
2025-11-01 00:12:46 +0900

    c

---
+++ src/operator.js
@@ -0,0 +1,30 @@
+// 산술 연산자
+// +, -, *, /, %, ++, --
+
+// 비교 연산자
+// ==, ===, !=, !==, >, <, >=, <=
+
+// 대입 연산자
+// = +=, -=, *=...
+
+// 연결 연산자 +
+
+// 논리 연산자
+// true flase
+// !, ||, && / NOT, OR, AND
+
+// null 병합 연산자
+// A ?? B
+
+let num1;
+let num2 = 10;
+
+console.log(num1 ?? 20);
+console.log(num2 ?? 20);
+
+// 삼항 연산자
+// A ? B : C
+
+let num = 100;
+let result = num % 2 === 0 ? "짝수" : "홀수";
+console.log(result);
