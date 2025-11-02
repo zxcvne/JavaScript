9ac7c0d913e39b94a8bab57a056d1461d9855ca0
zxcvne <nhs8421@gmail.com>
2025-11-03 00:12:12 +0900

    commit

---
+++ project2/src/App.js
@@ -0,0 +1,7 @@
+import TabBar from "./components/TabBar";
+import Content from "./components/Content";
+
+export default function App($app) {
+  const tabBar = new TabBar();
+  const content = new Content();
+}
