56a4b33098ec78de14af17e0b1704e92c2440ebf
zxcvne <nhs8421@gmail.com>
2025-11-03 19:49:10 +0900

    c

--- project2/src/App.js
+++ project2/src/App.js
@@ -2,6 +2,26 @@ import TabBar from "./components/TabBar";
 import Content from "./components/Content";
 
 export default function App($app) {
-  const tabBar = new TabBar();
+  this.state = {
+    // 현재 눌린 버튼의 값
+    currentTab: "all",
+    photos: [],
+  };
+  const tabBar = new TabBar({
+    initialState: "",
+    onClick: (name) => {
+      this.setState({
+        ...this.state,
+        currentTab: name,
+        photos,
+      });
+    },
+  });
   const content = new Content();
+
+  this.setState = (newState) => {
+    this.state = newState;
+    tabBar.setState(this.state.currentTab);
+    tabBar.setState(this.state.photos);
+  };
 }
